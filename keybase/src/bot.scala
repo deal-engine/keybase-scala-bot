3c702635353e446b98361d605c0b8412eaff47aa
Victor Hugo Borja <vborja@apache.org>
2020-03-29 14:27:36 -0600

    Start implementing keybase api

--- keybase/src/bot.scala
+++ keybase/src/bot.scala
@@ -1,5 +1,53 @@
 package keybase
+import os.Shellable
 import zio._
+
+case class WhoAmI(configured: Boolean,
+                  registered: Boolean,
+                  loggedIn: Boolean,
+                  sessionIsValid: Boolean,
+                  user: WhoAmI.User,
+                  deviceName: String)
+
+object WhoAmI {
+  case class User(uid: String, username: String)
+  object User {
+    implicit val rw = upickle.default.macroRW[User]
+  }
+
+  implicit val rw = upickle.default.macroRW[WhoAmI]
+}
+
+object cmd {
+
+  def whoami: IO[String, WhoAmI] = apply("whoami", "--json").map()
+
+  def apply(command: Shellable*): IO[String, String] = ZIO.fromEither {
+    os.proc("keybase", command: _*).call(check = false) match {
+      case r if r.exitCode == 0 =>
+        Right(r.out.text())
+      case r =>
+        Left(r.err.text())
+    }
+  }
+
+  def serviceStart: IO[String, String] =
+    apply(
+      "service",
+      "--label",
+      "keybase-ammonite-bot",
+      "--oneshot-username",
+      sys.env("KEYBASE_USERNAME"),
+      "--oneshot-paperkey",
+      sys.env("KEYBASE_PAPERKEY")
+    )
+
+  def chat(json: String): IO[String, String] = json_api("chat")(json)
+  def team(json: String): IO[String, String] = json_api("team")(json)
+
+  def json_api(api: String)(json: String): IO[String, String] =
+    apply(api, "api", "-m", json)
+}
 
 object bot extends zio.App {
 
